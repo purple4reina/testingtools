2e66774407e5fa0972ba49974365a77523c13251
Reina Abolofia <purple4reina@Reinas-MacBook-Pro.local>
2015-03-16 15:52:16 -0700

    Add two little helpful tools

---
+++ __init__.py
@@ -0,0 +1,27 @@
+import random
+import time
+
+
+def print_time_to_execute(fn):
+    """
+    Decorator that will print the time it took for a method to execute after
+    the method completes
+    """
+    def time_wrapper(*args, **kwargs):
+        start = time.time()
+        return_val = fn(*args, **kwargs)
+        end = time.time()
+        print 'Time to execute {}: {} seconds'.format(
+            fn.__name__, end - start)
+        return return_val
+    return time_wrapper
+
+
+def random_word(length=4, with_spaces=False):
+    """
+    Returns a random string of characters of a given length
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if with_spaces:
+        alphabet += ' '
+    return ''.join([random.choice(alphabet) for i in range(length)])
