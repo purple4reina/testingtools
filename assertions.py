5bde555c728888ec696bc1178ad71b9c48f1f94f
Reina B Abolofia <purple4reina@gmail.com>
2015-06-24 21:59:19 -0700

    Add assertRaises method

--- assertions.py
+++ assertions.py
@@ -1,7 +1,29 @@
 def assertEqual(*args):
-    for i in range(len(args)):
+    for i in xrange(len(args)):
         arg1 = args[i]
         arg2 = args[i - 1]
         if arg1 != arg2:
             raise AssertionError(
                 '{} != {}'.format(arg1, arg2))
+
+
+def assertRaises(exception_class, function, *funargs, **funkwargs):
+    """
+    Given an exception class and a function along with any args to the
+    function, assert that the exception is raised when the function is
+    executed.
+    """
+    try:
+        function(*funargs, **funkwargs)
+    except exception_class:
+        # everything worked as expected
+        return
+    except:
+        # some other error was raised, this case will be handled below
+        pass
+    raise AssertionError(
+        '{} with args {} and kwargs {} should have raised {}'.format(
+            function.__name__,
+            funargs or '()', funkwargs or '()',
+            exception_class.__name__)
+    )
